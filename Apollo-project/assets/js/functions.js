11176d75bc5bedeb8c667028f1077fc55711723d
priscillegnagne <prisc43@gmail.com>
2022-04-22 08:01:23 -0400

    ajout defilement et courriel pour formulaire

--- Apollo-project/assets/js/functions.js
+++ Apollo-project/assets/js/functions.js
@@ -1,3 +1,4 @@
+
 $( document ).ready(function() {
 	
 function nav(){
@@ -9,20 +10,40 @@ function nav(){
 	});
 	
 }
+window.addEventListener('DOMContentLoaded', (event) => {
 
 function smoothScroll(){
 // Faire la fonction ici
 }
 
+window.addEventListener('DOMContentLoaded', (event) => {
+
+const nombreImage = 3
+let imageActive = 1;
+const slidePrec = document.querySelector('.icon-chevron-thin-left');
+const slideSuiv = document.querySelector('.icon-chevron-thin-right');
+
 function fullSlider(){
 // Faire le code du slider ici
+	slideSuiv.addEventListener('click', function(){
+		if((imageActive * nombreImage) < 1){
+			imageActive++;
+		}
+	})
+	slidePrec.addEventListener('click', function(){
+		if(imageActive > 1){
+			imageActive--;
+		}
+	} )
 }
 
+fullSlider();
+})
 
 nav();
 
 smoothScroll();
 
-fullSlider();
 
 });
+})
